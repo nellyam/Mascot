63077250944a71216a98ee63bd3a4b57a78dab31
nellyam <nellynellyam@gmail.com>
2021-02-24 13:38:29 +0100

    projet mascot

---
+++ main.js
@@ -0,0 +1,31 @@
+let view = document.getElementById('view-button');
+let close = document.getElementById('close-button');
+let codey = document.getElementById('codey');
+
+let open = function() {
+  codey.style.display = 'block';
+  close.style.display = 'block';
+};
+
+let hide = function() {
+  codey.style.display = 'none';
+  close.style.display = 'none';
+};
+
+view.onclick = open;
+close.onclick = hide;
+
+// Write your code here
+let textChange = () => {
+  view.innerHTML = "Hello, World!";
+};
+
+let textReturn = () => {
+  view.innerHTML = "View";
+};
+
+
+view.addEventListener("click" , textChange);
+
+close.addEventListener("click" , textReturn);
+
